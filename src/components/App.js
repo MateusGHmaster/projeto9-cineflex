5fd64f109b12f458e4b692c3f82409bbc33b58c1
Mateus <mateusoliveira469@gmail.com>
2022-03-25 16:41:18 -0400

    initial-routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
-import Main from "./Main";
 import Header from "./Header";
+import Firstpage from "./Firstpage";
+import Session from "./Session";
+import Seats from "./Seats";
+import Success from "./Success";
+import Footer from "./Footer";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './style.css';
+
 
 export default function App () {
     return (
         
-        <>
+        <BrowserRouter>
             <Header />
-            <Main />
-        </>
+            <Routes>
+                <Route path="/" element={<Firstpage />} />
+                <Route path="/sessions/:movieID" element={<Session />} />
+                <Route path="/seats/:sessionID" element={<Seats />} />
+                <Route path="/success" element={<Success />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
 
     );
 }
--- src/components/Firstpage.js
+++ src/components/Firstpage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Main.css';
 
-export default function Main () {
+export default function Firstpage () {
     
     let mainPageTitle = 'Selecione o filme';
     
