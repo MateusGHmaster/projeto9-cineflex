bc2c67d6ed34aa71ece8cce04c0ec9dc74f17a7b
Mateus <mateusoliveira469@gmail.com>
2022-03-28 15:57:02 -0400

    success-visual-&&-footer-prototype-v1.0

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
-export default function Footer () {
+import './Footer.css'
+
+export default function Footer ({posterURL, title}) {
     
     return (
 
-        <>
-        </>
+        <section className='footer'>
+            <section className='footer-thumbnail'>
+                <img className='thumbnail' src={posterURL} alt={title} width={48} height={72}/>
+            </section>
+        </section>
 
     );
 }
--- src/components/Success.js
+++ src/components/Success.js
@@ -15,16 +15,16 @@ export default function Success ({title, date, name}) {
             <section className='success-page-title'>
                 <p className='order-success'>Pedido feito com sucesso!</p>
             </section>
-            <section className='movie-and-session'>
+            <section className='info-block'>
                 <span className='success-class'>Filme e sessão</span>
                 <p className='movie-ordered-name'>{title}</p>
                 <p className='session-ordered-data'>{date} {name}</p>
             </section>
-            <section className='ordered-seats'>
+            <section className='info-block'>
                 <span className='success-class'>Ingressos</span>
                 <p className='ordered-seat-number'>Assento(s) {}</p>
             </section>
-            <section className='buyer'>
+            <section className='info-block'>
                 <span className='success-class'>Comprador</span>
                 <p className='buyer-name'>Nome: {}</p>
                 <p className='buyer-cpf'>CPF: {}</p>
